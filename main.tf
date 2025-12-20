b178fb4ab711a751f2f1c71d98aabe55fea78e49
olisaemeka <olisaemkaokoroigwe@gmail.com>
2025-12-20 03:09:19 +0100

    added public ip and availbility zone

--- main.tf
+++ main.tf
@@ -18,6 +18,8 @@ resource "aws_vpc" "proVPC" {
 resource "aws_subnet" "proSubnet" {
   vpc_id     = aws_vpc.proVPC.id
   cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = us-east-2a
 
   tags = {
     Name = "Main_Subnet"
